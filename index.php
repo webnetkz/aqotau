cc16a1de2d6952439f7d581a1094bafce335be76
webnetkz <8gg001@gmail.com>
2019-06-15 13:07:57 +0600

    start project vse v prokat

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
     <head>
-        <title>AQ OTAU</title>
+        <title>Все в прокат</title>
 
         <meta charset="UTF-8">
         <meta name="theme-color" content="rgb(255, 255, 255)">
         <meta name="author" content="TOO WebNet">
-        <meta name="description" content="AQ OTAU">
+        <meta name="description" content="Vse V Prokat">
         <meta name="keywords" content="RedactorPhoto">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="index, follow">
 
-        <link rel="shortcut icon" href="/public/images/almatyholaLogo.png" type="image/png">
+        <link rel="shortcut icon" href="/public/images/miniLogo.png" type="image/png">
         <link rel="stylesheet" href="/public/styles/style.css">
         <link rel="stylesheet" href="/public/styles/mobileStyle.css">
         <link rel="manifest" href="/manifest.json">
@@ -19,97 +19,36 @@
     </head>
 
     <body>
-        <menu>
-            <p class="phone">+7 777 728 43 43</p>
-        </menu>
+        
         <header>
-            <div class="headerContent">
-                <h1>Дом торжеств AQ OTAU</h1>
-                <h2>Банкетный зал вместимостью до 300 человек</h2>
+            <div class="headerElement">
+                <img src="public/images/logo" alt="logotype Vse v prokat">
+            </div>
+            <div class="headerElement">
+                <p>
+                    <img src="public/images/plus.svg" class="icons" alt="append post">
+                    Добавить
+                </p>
+            </div>
+            <div class="headerElement">
+                <img src="public/images/search.svg" alt="search input">
             </div>
         </header>
 
-        <div class="container">
-            <div class="card">
-                <div class="cardItem">
-                    <img src="public/images/one.jpg" alt="toys hola" class="imgCard">
-                        <hr>
-                    <h4>Меню STANDART</h4>
-                    <p>
-                        3 вида холодных закусок /
-                        4 вида салата /
-                        2 горячих закусок /
-                        2 горячих блюд на выбор /
-                        фрукты /
-                        конфеты /
-                        пироженое /
-                        напитки, соки, мин.вода /
-                        водка, вино, коньяк /
-                        чай /
-                        хлебная корзина /
-                    </p>
-                    <h4>Цена: 10 000 НА ПЕРСОНУ</h4>
-                </div>
-            </div>
-            <div class="card">
-                <div class="cardItem">
-                    <img src="public/images/one.jpg" alt="toys hola" class="imgCard">
-                        <hr>
-                    <h4>Меню PREMIUM</h4>
-                    <p>
-                        3 вида холодных закусок /
-                        4 вида салата /
-                        2 горячих закусок /
-                        2 горячих блюд на выбор /
-                        фрукты /
-                        конфеты /
-                        пироженое /
-                        напитки, соки, мин.вода /
-                        водка, вино, коньяк /
-                        чай /
-                        хлебная корзина /
-                    </p>
-                    <h4>Цена: 13 000 НА ПЕРСОНУ</h4>
-                </div>
-            </div>
-            <div class="card">
-                <div class="cardItem">
-                    <img src="public/images/one.jpg" alt="toys hola" class="imgCard">
-                        <hr>
-                    <h4>Меню LUX</h4>
-                    <p>
-                        3 вида холодных закусок /
-                        4 вида салата /
-                        2 горячих закусок /
-                        2 горячих блюд на выбор /
-                        фрукты /
-                        конфеты /
-                        пироженое /
-                        напитки, соки, мин.вода /
-                        водка, вино, коньяк /
-                        чай /
-                        хлебная корзина /
-                    </p>
-                    <h4>Цена: 16 000 НА ПЕРСОНУ</h4>
-                </div>
-            </div>
+        <div id="search">
+        
         </div>
 
-        <div class="ps">
-
-
-        </div>
-
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4103.48435586223!2d76.91065865419041!3d43.34352486940171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skz!4v1559104162148!5m2!1sru!2skz" style="width: 100%;" height="450" frameborder="0" style="border:0" allowfullscreen>
-        </iframe>
-
-            <footer>
-                Контакты:<br>
-                АЛМАТЫ мкр.Карасу ул.Озёрная 2<br>
-                + 7 777 728 43 43<br>
-                + 7 727 299 43 88<br>
-                ДОМ ТОРЖЕСТВ AQ OTAU
-            </footer>
+        <nav>
+            <div class="categoriesElement"></div>
+            <div class="categoriesElement"></div>
+            <div class="categoriesElement"></div>
+            <div class="categoriesElement"></div>
+            <div class="categoriesElement"></div>
+            <div class="categoriesElement"></div>
+            <div class="categoriesElement"></div>
+            <div class="categoriesElement"></div>
+        </nav>
 
         <script>
              // Проверка на поддержку service worker
